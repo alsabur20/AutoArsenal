c56860207afe0bf4fe0e8376f192f970437a39bd
Abdul Sabur <alsabur20@gmail.com>
2024-04-27 12:36:04 +0500

    Working on Employees

---
+++ AutoArsenal-App/AutoArsenal-App/Pages/Manager/Employees.cshtml.cs
@@ -0,0 +1,101 @@
+using AutoArsenal_App.Controllers;
+using AutoArsenal_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AutoArsenal_App.Pages.Manager
+{
+    public class EmplyeesModel : PageModel
+    {
+        //for creating new
+        [BindProperty]
+        public Person Person { get; set; }
+        [BindProperty]
+        public Employee Employee { get; set; }
+        [BindProperty]
+        public Location Location { get; set; }
+
+        //for viewing
+        [BindProperty]
+        public List<string> Cities { get; set; }
+        [BindProperty]
+        public List<string> Provinces { get; set; }
+        [BindProperty]
+        public List<Lookup> Lookups { get; set; }
+        [BindProperty]
+        public List<Person> Persons { get; set; }
+        [BindProperty]
+        public List<Employee> Employees { get; set; }
+        public async void OnGet()
+        {
+            Cities = new List<string> {
+            "Karachi",
+            "Lahore",
+            "Islamabad",
+            "Faisalabad",
+            "Rawalpindi",
+            "Multan",
+            "Gujranwala",
+            "Peshawar",
+            "Quetta",
+            "Sialkot",
+            "Hyderabad",
+            "Bahawalpur",
+            "Sargodha",
+            "Abbottabad",
+            "Sukkur",
+            "Nawabshah",
+            "Mingora",
+            "Mirpur Khas",
+            "Sheikhupura",
+            "Jhang"};
+            Provinces = new List<string>{
+            "Punjab",
+            "Sindh",
+            "Khyber Pakhtunkhwa",
+            "Balochistan",
+            "Gilgit-Baltistan" };
+            try
+            {
+                Lookups = await LookupController.GetLookup();
+                Persons = await PersonController.GetPerson();
+                Employees = await EmployeeController.GetEmployee();
+
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorOnServer"] = ex.Message;
+            }
+        }
+        public async Task<IActionResult> OnPostAddEmployee()
+        {
+            int peronId = -1;
+            int locationId = -1;
+            try
+            {
+                if (Location != null)
+                {
+                    await LocationController.AddLocation(Location);
+                    locationId = await LocationController.GetLocationId(Location);
+                }
+                if (Person != null)
+                {
+                    Person.LocationId = locationId;
+                    await PersonController.AddPerson(Person);
+                    peronId = await PersonController.GetPersonId(Person);
+                }
+                if (Employee != null)
+                {
+                    Employee.Id = peronId;
+                    await EmployeeController.AddEmployee(Employee);
+                }
+                return RedirectToPage("/Manager/Employees");
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorOnServer"] = ex.Message;
+                return RedirectToPage("/Manager/Employees");
+            }
+        }
+    }
+}
