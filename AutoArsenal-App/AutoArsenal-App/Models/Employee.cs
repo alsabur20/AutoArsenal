e5972c7b13472617c0baaa4747f59e0340d0f469
Abdul Sabur <alsabur20@gmail.com>
2024-04-27 12:35:30 +0500

    Models made

---
+++ AutoArsenal-App/AutoArsenal-App/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoArsenal_App.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public DateTime? JoiningDate { get; set; }
+        public int? Role { get; set; }
+        public int? CredentialsId { get; set; }
+        public double? Salary { get; set; }
+    }
+}
