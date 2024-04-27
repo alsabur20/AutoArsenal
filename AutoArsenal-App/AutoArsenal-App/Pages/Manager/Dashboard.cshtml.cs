d7bf6981c590c32a1e2c01713dec9a690fc4bd80
Abdul Sabur <alsabur20@gmail.com>
2024-04-27 17:00:12 +0500

    modified dashboard

--- AutoArsenal-App/AutoArsenal-App/Pages/Manager/Dashboard.cshtml.cs
+++ AutoArsenal-App/AutoArsenal-App/Pages/Manager/Dashboard.cshtml.cs
@@ -7,22 +7,8 @@ namespace AutoArsenal_App.Pages.Manager
 {
     public class DashboardModel : PageModel
     {
-        public required List<Lookup> Lookup { get; set; }
         public async void OnGet()
         {
-            try
-            {
-                Lookup = await LookupController.GetLookup();
-                foreach (Lookup lookup in Lookup)
-                {
-                    Console.WriteLine(lookup.Value);
-                }
-            }
-            catch (Exception ex)
-            {
-                TempData["ErrorOnServer"] = ex.Message;
-                Console.WriteLine(ex.Message + ex.StackTrace);
-            }
         }
     }
 }
