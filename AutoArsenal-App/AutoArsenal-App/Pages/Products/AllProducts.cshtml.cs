4b7ccbed0356496f562b9b8e73be16b817e4f860
AbdulRehman7590 <theleostar3@gmail.com>
2024-05-02 00:14:18 +0500

    Working on products CRUD

--- AutoArsenal-App/AutoArsenal-App/Pages/Products/AllProducts.cshtml.cs
+++ AutoArsenal-App/AutoArsenal-App/Pages/Products/AllProducts.cshtml.cs
@@ -2,35 +2,74 @@ using AutoArsenal_App.Controllers;
 using AutoArsenal_App.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AutoArsenal_App.Pages.Products
 {
     public class ProductsModel : PageModel
     {
+        // for deleting
+        [BindProperty]
+        public int DeleteID { get; set; }
 
-
-        // For Viewing
+        // for viewing
+        [BindProperty]
         public List<Product> Products { get; set; }
+
+        [BindProperty]
         public List<ProductCategory> ProductCategories { get; set; }
+
+        [BindProperty]
         public List<Lookup> Lookups { get; set; }
-        public List<Inventory> Inventories { get; set; }
+
+        [BindProperty]
         public List<Manufacturer> Manufacturers { get; set; }
 
-        public async void OnGet()
+        public async Task OnGetAsync()
         {
             try
             {
                 Lookups = await LookupController.GetLookup();
                 Products = await ProductController.GetProducts();
                 ProductCategories = await ProductCategoryController.GetProductCategory();
-                Inventories = await InventoryController.GetInventory();
                 Manufacturers = await ManufacturerController.GetManufacturers();
-
             }
             catch (Exception ex)
             {
                 TempData["ErrorOnServer"] = ex.Message;
             }
         }
+
+        public async Task<IActionResult> OnPostUpdateProductAsync(Product product)
+        {
+            try
+            {
+                
+                return Page();
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorOnServer"] = ex.Message;
+                return Page();
+            }
+        }
+
+        public async Task<IActionResult> OnPostDeleteProductAsync()
+        {
+            try
+            {
+                
+                return Page();
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorOnServer"] = ex.Message;
+                return Page();
+            }
+        }
+
+
     }
 }
