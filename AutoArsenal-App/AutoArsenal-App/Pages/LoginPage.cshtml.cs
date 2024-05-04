cf16f5a93c125fe258b47b7dc70cb8f05d331261
Faisal Ilyas <faisalilyas2005@gmail.com>
2024-05-04 16:34:09 +0500

    message

---
+++ AutoArsenal-App/AutoArsenal-App/Pages/LoginPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AutoArsenal_App.Pages
+{
+    public class LoginPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
