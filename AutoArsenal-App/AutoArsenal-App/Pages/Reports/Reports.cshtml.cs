53263efc2eee26b44ca9c1696ff7604828936573
Abdul Sabur <alsabur20@gmail.com>
2024-05-09 23:08:09 +0500

    working on reoports pafe

--- AutoArsenal-App/AutoArsenal-App/Pages/Reports/Reports.cshtml.cs
+++ AutoArsenal-App/AutoArsenal-App/Pages/Reports/Reports.cshtml.cs
@@ -1,3 +1,5 @@
+using AutoArsenal_App.Controllers;
+using AutoArsenal_App.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -7,8 +9,27 @@ namespace AutoArsenal_App.Pages.Reports
     [Authorize(Roles = "Manager")]
     public class ReportsModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public List<Sale> Sales { get; set; }
+        [BindProperty]
+        public List<SaleDetails> SaleDetails { get; set; }
+        [BindProperty]
+        public List<Product> Products { get; set; }
+        [BindProperty]
+        public List<ProductCategory> ProductCategories { get; set; }
+        public async void OnGet()
         {
+            try
+            {
+                Products = await ProductController.GetProducts();
+                ProductCategories = await ProductCategoryController.GetProductCategories();
+                Sales = await SaleController.GetSales();
+                SaleDetails = await SaleDetailsController.GetSaleDetails();
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorOnServer"] = ex.Message;
+            }
         }
     }
 }
